aa7b95092e526bf62e03160de74a576acdb034e2
NatyCa24 <nataliacardoso@prof.educacao.sp.gov.br>
2024-05-28 11:25:24 -0300

    alterando arquivo js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,36 @@
+ //variaveisRaquete
+let xRaquete = 5
+let yRaquete = 150
+let baseRaquete = 10
+let alturaRaquete = 90
+
+//variaveisdaBolinha
+let xBolinha = 300
+let yBolinha = 200
+let diametro = 30
+
+//velocidadeBolinha
+let velocidadexBolinha = 5
+let velocidadeyBolinha = 5
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background("black");
+  mostraBolinha()
+  movimentoBolinha()
+  verificacaoColisaoBorda()
+  mostraRaquete()
+  movimentoRaquete()
+ 
+  }
+
+function movimentoRaquete(){
+  if(keyIsDown(UP_ARROW))
+    yRaquete -= 10
+  if(keyIsDown(DOWN_ARROW))
     yRaquete += 10
 }
 function mostraRaquete(){
