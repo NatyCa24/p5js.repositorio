a8caa3f7f2f555632448bf57657780fdba1c5727
NatyCa24 <nataliacardoso@prof.educacao.sp.gov.br>
2024-05-28 11:18:08 -0300

    arquivo js

---
+++ sketch.js
@@ -0,0 +1,20 @@
+    yRaquete += 10
+}
+function mostraRaquete(){
+   rect(xRaquete,yRaquete,baseRaquete,alturaRaquete)
+}
+  function mostraBolinha(){
+    circle(xBolinha,yBolinha,diametro)
+  }
+  
+  function movimentoBolinha(){
+  xBolinha += velocidadexBolinha
+  yBolinha += velocidadeyBolinha
+    }
+
+  function verificacaoColisaoBorda(){
+  if(xBolinha +15 > width || xBolinha -15 < 0)
+    velocidadexBolinha *= -1
+  if(yBolinha +15 > height || yBolinha -15 < 0)
+    velocidadeyBolinha *= -1 
+}
